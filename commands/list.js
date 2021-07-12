775639ff350ed450ad41ca525c2ddfa3a27aac6a
Gab-sys <gabriel.program007@gmail.com>
2021-07-12 22:11:47 +0200

    Commands for mod or admin use only
    Command !list know is locked so only people with special role can use it.

--- commands/list.js
+++ commands/list.js
@@ -2,10 +2,14 @@ module.exports = {
     name: "list",
     description: "Shows the list of all users in the database.",
     async execute(message, args, Users){
-        //getting all users in the database
-        const UserList = await Users.findAll({ });
+        if(message.member.roles.cache.has("847552231288602664"))
+        {
+            //getting all users in the database
+            const UserList = await Users.findAll({ });
         
-        const Userstring = UserList.map(t => t.username + '\n - hugs = '+ t.hugs + '\n - punches = ' + t.punches ).join('\n') || 'No User set.';
-        return message.channel.send(`${Userstring}`, {code: true});
+            const Userstring = UserList.map(t => t.username + '\n - hugs = '+ t.hugs + '\n - punches = ' + t.punches ).join('\n') || 'No User set.';
+            return message.channel.send(`${Userstring}`, {code: true});
+        }
+        else return message.reply(`Only users with a special role can use this command.`);
     }
 }
