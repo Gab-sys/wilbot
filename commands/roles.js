0573f6e77f5d0cfdf3023c36dfa83a4a1f9ec601
Gab-sys <gabriel.program007@gmail.com>
2021-07-12 22:59:05 +0200

    Bug fux: cant get roles
    Fix a bug where user cant get role even tho user has enough punches or hugs

--- commands/roles.js
+++ commands/roles.js
@@ -13,65 +13,61 @@ module.exports = {
     }
 }
 function CheckHug(message, hugs) {
-    switch (hugs) {
-        case 10:
-            message.member.roles.add("864241081986056252");
-            message.channel.send("You have 10 hugs! Here, take this role.");
-            break;
-        case 100:
-            message.member.roles.add("864240193028358184");
-            message.channel.send("You have 100 hugs! Here, take this role.");
-            break;
-        case 1000:
-            message.member.roles.add("864240359580368957");
-            message.channel.send("You have 1 000 hugs! Here, take this role.");
-            break;
-        case 10000:
-            message.member.roles.add("864240492609667092");
-            message.channel.send("You have 10 000 hugs! Here, take this role.");
-            break;
-        case 100000:
-            message.member.roles.add("864240622633615380");
-            message.channel.send("You have 100 000 hugs! Here, take this role.");
-            break;
-        case 1000000:
-            message.member.roles.add("864240837764448316");
-            message.channel.send("You have 1 000 000 hugs! Here, take this role.");
-            break;
-        default:
-            message.channel.send("You don't have enough hugs to get a hug role.");
-            break;
+    if(hugs < 10){
+        message.member.roles.add("864241081986056252");
+        message.channel.send("You have 10 or more hugs! Here, take this role.");
     }
+    else if(hugs > 100 || hugs < 1000){
+        message.member.roles.add("864240193028358184");
+        message.channel.send("You have 100 or more hugs! Here, take this role.");
+    }
+    else if(hugs > 1000 || hugs < 10000){
+        message.member.roles.add("864240359580368957");
+        message.channel.send("You have 1000 or more hugs! Here, take this role.");
+    }
+    else if(hugs > 10000 || hugs < 100000){
+        message.member.roles.add("864240492609667092");
+        message.channel.send("You have 10 000 or more hugs! Here, take this role.");
+    }
+    else if(hugs > 100000 || hugs < 1000000){
+        message.member.roles.add("864240622633615380");
+        message.channel.send("You have 100 000 or more hugs! Here, take this role.");
+    }
+    else if(hugs > 1000000){
+        message.member.roles.add("864240837764448316");
+        message.channel.send("You have 1 000 000 or more hugs! Here, take this role.");
+    }
+    else{
+        message.channel.send("You don't have enough hugs to get a punch role.");
+    }   
 }
 
 function CheckPunch(message, punches) {
-    switch (punches) {
-        case 10:
-            message.member.roles.add("864241204535623750");
-            message.channel.send("You have 10 punches! Here, take this role.");
-            break;
-        case 100:
-            message.member.roles.add("864241326883209247");
-            message.channel.send("You have 100 punches! Here, take this role.");
-            break;
-        case 1000:
-            message.member.roles.add("864241443575693332");
-            message.channel.send("You have 1000 punches! Here, take this role.");
-            break;
-        case 10000:
-            message.member.roles.add("864241546844045333");
-            message.channel.send("You have 10 000 punches! Here, take this role.");
-            break;
-        case 100000:
-            message.member.roles.add("864241674725097523");
-            message.channel.send("You have 100 000 punches! Here, take this role.");
-            break;
-        case 1000000:
-            message.member.roles.add("864241798997999626");
-            message.channel.send("You have 1 000 000 punches! Here, take this role.");
-            break;
-        default:
-            message.channel.send("You don't have enough punches to get a punch role.");
-            break;
+    if(punches < 10){
+        message.member.roles.add("864241204535623750");
+        message.channel.send("You have 10 or more punches! Here, take this role.");
     }
+    else if(punches > 100 || punches < 1000){
+        message.member.roles.add("864241326883209247");
+        message.channel.send("You have 100 or more punches! Here, take this role.");
+    }
+    else if(punches > 1000 || punches < 10000){
+        message.member.roles.add("864241443575693332");
+        message.channel.send("You have 1000 or more punches! Here, take this role.");
+    }
+    else if(punches > 10000 || punches < 100000){
+        message.member.roles.add("864241546844045333");
+        message.channel.send("You have 10 000 or more punches! Here, take this role.");
+    }
+    else if(punches > 100000 || punches < 1000000){
+        message.member.roles.add("864241674725097523");
+        message.channel.send("You have 100 000 or more punches! Here, take this role.");
+    }
+    else if(punches > 1000000){
+        message.member.roles.add("864241798997999626");
+        message.channel.send("You have 1 000 000 or more punches! Here, take this role.");
+    }
+    else{
+        message.channel.send("You don't have enough punches to get a punch role.");
+    }   
 }
